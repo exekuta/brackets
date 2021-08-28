5633d33669c49c20770377484b1a9c75b9408a6e
unknown <exekuta@gmail.com>
2021-08-29 00:40:36 +0300

    feat: 1st version of solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  const bracketsOpen = ['(', '{', '[', '|', '1', '3', '5', '7', '8'];
+  const bracketsPair = {
+    [')'] : '(',
+    ['}'] : '{',
+    [']'] : '[',
+    ['|'] : '|',
+    ['2'] : '1',
+    ['4'] : '3',
+    ['6'] : '5',
+    ['7'] : '7',
+    ['8'] : '8',
+  };
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+
+    if (bracketsOpen.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let topElement = stack[stack.length - 1];
+
+      if (bracketsPair[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+  
 }
+console.log(check('||'));
